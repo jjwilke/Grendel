0f037a56cac732489bf2be5d1ee747242dfd46f3
Jeremiah Wilke <jjwilke@jkenny.ccqc.uga.edu>
2010-08-27 15:27:15 -0400

    added example config script

---
+++ runme.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env tcsh
+
+git submodule init
+git submodule update
+aclocal
+glibtoolize
+automake --add-missing
+automake
+autoconf
+
+./configure --prefix=/opt/gigide/3.0 --with-cxx=g++ --with-libdirs="-L/usr/lib" --with-cc=gcc --with-f77=gfortran --with-cxx-flags="-O2 -g" --with-intder=$HOME/Programs/Intder/intder --with-libs='-lblas -llapack' --with-anharm=$HOME/Programs/Anharm/anharm
+
